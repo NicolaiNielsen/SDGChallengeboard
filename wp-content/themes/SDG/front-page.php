049cc010aa71462fefa286b116ee3a50c3d5d43d
NicolaiNielsen <Nicolaiaphichat@gmail.com>
2020-02-24 10:41:38 +0100

    rightside

--- wp-content/themes/SDG/front-page.php
+++ wp-content/themes/SDG/front-page.php
@@ -1,5 +1,16 @@
 <?php get_header();?>
 
+    <div class="container">
+        <div class="row">
+                <div class="col">
+                    left side
+                </div>
 
+                <div class="col">
+                    rightside
+                </div>
+        </div>
+
+    </div>
 
 <?php get_footer();?>
--- wp-content/themes/SDG/functions.php
+++ wp-content/themes/SDG/functions.php
@@ -6,3 +6,4 @@ function load_stylesheets()
     wp_register_style('stylesheet', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
     wp_enqueue_style('stylesheet');
 }
+add_action('wp_enqueue_script', 'load_stylesheets');
