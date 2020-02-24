02024f61362f2fdcaf4e04c8721bb14237815008
NicolaiNielsen <Nicolaiaphichat@gmail.com>
2020-02-24 11:05:10 +0100

    tetst

--- wp-content/themes/SDG/functions.php
+++ wp-content/themes/SDG/functions.php
@@ -3,10 +3,11 @@
 
 function load_stylesheets()
 {
-    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
+    wp_register_style('bootstrap', get_template_directory_uri() . 'C:\xampp\htdocs\SDGChallengeboard\wp-content\themes\SDG\css\bootsrap.min.css', array(), false, 'all');
     wp_enqueue_style('bootstrap');
 
-    wp_register_style('style', get_template_directory_uri() . '/style.css', array(), false, 'all');
+    wp_register_style('style', get_template_directory_uri() . 'C:\xampp\htdocs\SDGChallengeboard\wp-content\themes\SDG\style.css', array(), false, 'all');
     wp_enqueue_style('style');
 }
+
 add_action('wp_enqueue_script', 'load_stylesheets');
