40ad647129a95262b7e8a3e323f596c0b77fd378
NicolaiNielsen <Nicolaiaphichat@gmail.com>
2020-02-25 10:39:36 +0100

    images

--- wp-content/themes/SDG/front-page.php
+++ wp-content/themes/SDG/front-page.php
@@ -4,8 +4,27 @@
 
         <h1 align="center">The Challenge board</h1>
         <h2 align="center">Creating collaboration between the worldsleading organisations and the minds of tomorrow</h2>
-            
-        <img src="img/1.jpg">
+
+        <div id="imageList">
+            <img class="FN1" src="/SDGChallengeboard/wp-content/themes/SDG/img/1.jpg" alt="FN 1">
+            <img class="FN2" src="/SDGChallengeboard/wp-content/themes/SDG/img/2.jpg" alt="FN 2">
+            <img class="FN3" src="/SDGChallengeboard/wp-content/themes/SDG/img/3.jpg" alt="FN 3">
+            <img class="FN4" src="/SDGChallengeboard/wp-content/themes/SDG/img/4.jpg" alt="FN 4">
+            <img class="FN5" src="/SDGChallengeboard/wp-content/themes/SDG/img/5.jpg" alt="FN 5">
+            <img class="FN6" src="/SDGChallengeboard/wp-content/themes/SDG/img/6.jpg" alt="FN 6">
+            <img class="FN7" src="/SDGChallengeboard/wp-content/themes/SDG/img/7.jpg" alt="FN 7">
+            <img class="FN8" src="/SDGChallengeboard/wp-content/themes/SDG/img/8.jpg" alt="FN 8">
+            <img class="FN9" src="/SDGChallengeboard/wp-content/themes/SDG/img/9.jpg" alt="FN 9">
+            <img class="FN10" src="/SDGChallengeboard/wp-content/themes/SDG/img/10_1.jpg" alt="FN 10_1">
+            <img class="FN11" src="/SDGChallengeboard/wp-content/themes/SDG/img/11.jpg" alt="FN 11">
+            <img class="FN12" src="/SDGChallengeboard/wp-content/themes/SDG/img/12.jpg" alt="FN 12">
+            <img class="FN13" src="/SDGChallengeboard/wp-content/themes/SDG/img/13.jpg" alt="FN 13">
+            <img class="FN14" src="/SDGChallengeboard/wp-content/themes/SDG/img/14.jpg" alt="FN 14">
+            <img class="FN15" src="/SDGChallengeboard/wp-content/themes/SDG/img/15.jpg" alt="FN 15">
+            <img class="FN16" src="/SDGChallengeboard/wp-content/themes/SDG/img/16.jpg" alt="FN 16">
+            <img class="FN17" src="/SDGChallengeboard/wp-content/themes/SDG/img/17.jpg" alt="FN 17">
+        </div>
+
 
         <div class="row">
 
