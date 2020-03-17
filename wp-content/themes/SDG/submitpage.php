4f9a7ba102ac0b5763bc512417242039c43f492b
NicolaiNielsen <Nicolaiaphichat@gmail.com>
2020-03-17 05:07:03 +0100

    check pages

---
+++ wp-content/themes/SDG/submitpage.php
@@ -0,0 +1,28 @@
+<?php /* Template Name: submitpage */ ?>
+
+<?php get_header();?>
+
+    <div class="container pt-2 pb-2">
+
+        <h1 align="center"><?php the_title();?></h1>
+
+        <?php if (have_posts()) : while(have_posts()) : the_post();?>
+
+            <?php the_content();?>
+
+        <?php endwhile; endif;?>
+
+    </div>
+
+
+    <div class="container2 pt-2 pb-2">
+        <h4 align="center">In September 2015, 193 world leaders agreed to 17 Global Goals for Sustainable Development. If <br> 
+        these Goals are completed, it would mean an end to extreme poverty, inequality and climate change <br> by 2030.</h4>
+    </div>
+    
+    <div class="container">
+        <a href ="#"> Button </a>
+    </div>
+    
+
+<?php get_footer();?>
