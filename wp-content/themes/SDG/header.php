60faf9dc0ee05cf07e4a9ee58021ff8d0cd009c9
NicolaiNielsen <Nicolaiaphichat@gmail.com>
2020-03-17 11:51:18 +0100

    custom submit form

--- wp-content/themes/SDG/header.php
+++ wp-content/themes/SDG/header.php
@@ -21,12 +21,3 @@
 
 
 
-<!-- <?php wp_nav_menu (
-
-// array(
-
-//     'theme_location' => 'top-menu',
-//     'menu_class' => 'navigation'
-// )
-
-);?> -->
--- wp-content/themes/SDG/submitpage.php
+++ wp-content/themes/SDG/submitpage.php
@@ -2,12 +2,19 @@
 
 <?php get_header();?>
 
+
+    <div class="bg-modal">
+        <div class="modal-content">
+        <?php if (function_exists('user_submitted_posts')) user_submitted_posts(); ?>
+        </div>
+
+    </div>
     <div class="container pt-2 pb-2">
 
         <h1 align="center"><?php the_title();?></h1>
 
 
-        <?php if (function_exists('user_submitted_posts')) user_submitted_posts(); ?>
+        
 
     </div>
 
